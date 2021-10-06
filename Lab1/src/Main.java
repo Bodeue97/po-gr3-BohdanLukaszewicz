2ca9780831e03284638ac03e3249c6389d2baa4c
Bodeue97 <lukaszebo@gmail.com>
2021-10-06 14:59:23 +0200

    zad9

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -3,26 +3,26 @@ public class Main {
     public static void main(String[] args) {
         //Zad1
 
-        System.out.println(31+29+31);
+        System.out.println(31 + 29 + 31);
         //Zad2
         int suma = 0;
-        for (int i = 1; i <=10; i++){
-            suma+=i;
+        for (int i = 1; i <= 10; i++) {
+            suma += i;
 
         }
         System.out.println(suma);
         //Zad3
         int iloczyn = 1;
-        for (int i = 1; i <=10; i++){
-            iloczyn*=i;
+        for (int i = 1; i <= 10; i++) {
+            iloczyn *= i;
 
         }
         System.out.println(iloczyn);
         //Zad4
         float stanKonta = 1000;
-        for (int i = 1; i <=3; i++){
-            stanKonta+=stanKonta*0.06;
-            System.out.println("Saldo po " + i + " roku/latach wynosi "+stanKonta);
+        for (int i = 1; i <= 3; i++) {
+            stanKonta += stanKonta * 0.06;
+            System.out.println("Saldo po " + i + " roku/latach wynosi " + stanKonta);
 
 
         }
@@ -31,7 +31,7 @@ public class Main {
         System.out.println("______\n|JAVA|\n------");
 
         //Zad6
-        System.out.println("   /////  \n  +\"\"\"\"\"+\n(| o   o |)\n  |  ^  |\n  | `_\' |\n  +-----+");
+        System.out.println("   /////  \n  +\"\"\"\"\"+\n(| o   o |)\n  |  ^  |\n  | `_' |\n  +-----+");
         //Zad7
 
         System.out.println("" +
@@ -46,7 +46,35 @@ public class Main {
 
 
         //Zad8
-        System.out.println("    +    \n   + +   \n  +   +  \n +-----+ \n |     | \n | .-. | \n | | | | \n +-+-+-+ ");
+        System.out.println("    +    \n" +
+                "   + +   \n  +   +  \n" +
+                " +-----+ \n |     | \n" +
+                " | .-. | \n | | | | \n" +
+                " +-+-+-+ ");
+
+        //Zad9
+
+
+        System.out.println("" +
+                "                     __________   \n" +
+                "                    /          \\   \n" +
+                "                   (    Witaj   )\n" +
+                "                   (            )\n" +
+                "                    \\____  ____/\n" +
+                "                        / /\n" +
+                "   (-)   /\\ /\\   (-)  <=_/\n" +
+                "     \\\\ (* ^ *) //\n" +
+                "      \\\\ ~~~~~ //\n" +
+                "        |~~~~~|\n" +
+                "        \\~~~~~/\n" +
+                "         | | |\n" +
+                "         | | |\n" +
+                "        (__|__)");
+
+        //Zad10
+
+
+
     }
 
 }
