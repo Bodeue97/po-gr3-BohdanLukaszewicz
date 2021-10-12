d13d8310d0ab33a3d820e0cc283ed41a74cfa4a4
Bodeue97 <lukaszebo@gmail.com>
2021-10-12 13:24:49 +0200

    wiecej zadan

--- Lab2/src/pl/bohdanlukaszewicz/Main.java
+++ Lab2/src/pl/bohdanlukaszewicz/Main.java
@@ -69,6 +69,68 @@ public class Main {
             wynik+=(pow(-1, i+1)*liczby[i])/factorial;
         }
         System.out.println(wynik);
+        //1.2
+        float[] liczby2 = new float[10];
+        float n = liczby[0];
+        for(int i = 0; i<=liczby.length-2; i++) {
+            liczby2[i] = liczby[i+1];
+
+        }
+        System.out.println("\n\n\n");
+        liczby2[liczby2.length-1] = n;
+        for(int i = 0; i <=liczby2.length-1; i++){
+            System.out.println(liczby2[i]);
+        }
+
+        //Zad2
+        //2.1
+            //a
+        int[] naturalne = {1, 2, 3, 4, 5,100, 6, 7, 8, 9, 10};
+        int counter = 0;
+        for(int i = 0; i<=naturalne.length-1; i++){
+            if(naturalne[i] % 2 == 0) {
+                counter++;
+                }
+            }
+        System.out.println("\n\n\n\n\n\n\n"+counter+"\n");
+            //b
+        counter = 0;
+        for(int i = 0; i<=naturalne.length-1; i++){
+            if(naturalne[i] % 3 == 0 && naturalne[i] % 5 != 0) {
+                counter++;
+            }
+        }
+        System.out.println(counter+"\n");
+
+        //c
+        counter = 0;
+        for(int i = 0; i<=naturalne.length-1; i++){
+            if(sqrt(naturalne[i]) % 2 == 0) {
+                counter++;
+            }
+        }
+        System.out.println(counter+"\n");
+        //d
+        counter = 0;
+        for(int i = 1; i<=naturalne.length-2; i++){
+            if(naturalne[i] < (naturalne[i-1]+naturalne[i+1])/2) {
+                counter++;
+            }
+        }
+        System.out.println(counter+"\n");
+
+        //e
+        counter = 0;
+        factorial = 1;
+        for(int i = 1; i<=naturalne.length-1; i++){
+            factorial*=i;
+            if (pow(2, i) < naturalne[i] && naturalne[i] < factorial) {
+                counter++;
+            }
+        }
+        System.out.println(counter+"\n");
+        //f
+
 
     }
 }
