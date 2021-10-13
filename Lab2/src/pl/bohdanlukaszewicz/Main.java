c0858089aa093f3450d79642794327e0f3b46669
Bodeue97 <lukaszebo@gmail.com>
2021-10-13 12:20:35 +0200

    zrobione

--- Lab2/src/pl/bohdanlukaszewicz/Main.java
+++ Lab2/src/pl/bohdanlukaszewicz/Main.java
@@ -111,6 +111,139 @@ public class Main {
 //            System.out.println(liczby2[i]);
 //        }
 
+        //Zad2
+        //2.1
+        //a
+        int counter = 0;
+//
+//        for(int i = 0; i<=liczby.length-1; i++){
+//            liczby[i] = scn.nextDouble();
+//            if(liczby[i] %2 != 0)
+//            counter++;
+//       }
+//        System.out.println(counter);
+
+        //b
+
+//
+//        for(int i = 0; i<=liczby.length-1; i++){
+//            liczby[i] = scn.nextDouble();
+//            if(liczby[i] %3 == 0 && liczby[i]%5!=0)
+//                counter++;
+//       }
+//        System.out.println(counter);
+
+        //c
+
+//        for(int i = 0; i<=liczby.length-1; i++){
+//            liczby[i] = scn.nextDouble();
+//            if(sqrt(liczby[i]) %2 == 0)
+//            counter++;
+//       }
+//        System.out.println(counter);
+
+        //d
+//        for(int i = 0; i<=liczby.length-1; i++) {
+//            liczby[i] = scn.nextDouble();
+//
+//        }
+//        for(int i = 1; i<=liczby.length-2; i++){
+//            if(liczby[i] < (liczby[i-1]+liczby[i+1])/2)
+//                counter++;
+//        }
+//          System.out.println(counter);
+
+        //e
+//        int factorial = 1;
+//        for (int i = 0; i <= liczby.length - 1; i++) {
+//            liczby[i] = scn.nextDouble();
+//
+//
+//        }
+//        for (int i = 1; i <= liczby.length - 1; i++) {
+//            factorial *= i;
+//            if (pow(2, i) <= liczby[i] && liczby[i] <= factorial)
+//                counter++;
+//        }
+//        System.out.println(counter);
+
+        //f
+//        for(int i = 0; i<=liczby.length-1; i++){
+//            liczby[i] = scn.nextDouble();
+//            if(i+1%2 != 0 && liczby[i]%2==0)
+//                counter++;
+//       }
+//        System.out.println(counter);
+
+//        for(int i = 0; i<=liczby.length-1; i++){
+//            liczby[i] = scn.nextDouble();
+//            if(liczby[i] %2 !=0 && liczby[i] >=0)
+//                counter++;
+//        }
+//        System.out.println(counter);
+//        for(int i = 0; i<=liczby.length-1; i++){
+//            liczby[i] = scn.nextDouble();
+//            if(abs(liczby[i]) < pow(i, 2))
+//                counter++;
+//        }
+//        System.out.println(counter);
+
+        //2.2
+//        for(int i = 0; i<=liczby.length-1; i++){
+//            liczby[i] = scn.nextDouble();
+//            if(liczby[i] > 0)
+//                counter+=2*liczby[i];
+//        }
+//        System.out.println(counter);
+        //2.3
+//        int zeros = 0, plus = 0, minus = 0;
+//        for(int i = 0; i<=liczby.length-1; i++){
+//            liczby[i] = scn.nextDouble();
+//            if(liczby[i] == 0)
+//                zeros++;
+//            if(liczby[i] < 0)
+//                minus++;
+//            if(liczby[i] > 0)
+//                plus++;
+//        }
+//        System.out.println(counter);
+        //2.4
+//        double max, min;
+//
+//        for(int i = 0; i<=liczby.length-1; i++){
+//            liczby[i] = scn.nextDouble();
+//
+//        }
+//        min = liczby[0];
+//        max = liczby[0];
+//        for(int i = 0; i<=liczby.length-1;i++)
+//                if(i > 0) {
+//                    if (liczby[i] > max)
+//                        max = liczby[i];
+//                    if(liczby[i] < min)
+//                        min = liczby[i];
+//                }
+//
+//        System.out.println(min+ "\n" + max);
+
+        //2.5
+
+//        for(int i = 0; i<=liczby.length-1; i++){
+//            liczby[i] = scn.nextDouble();
+//
+//        }
+//        for(int i = 0; i<=liczby.length-2;i++){
+//
+//
+//            if(liczby[i] > 0 && liczby[i+1] > 0)
+//                counter++;
+//        }
+//
+//        System.out.println(counter);
+
 
     }
+
+
+
 }
